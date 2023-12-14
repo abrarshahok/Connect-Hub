1998d9a04b0f743e30ba39abe8d7407ad5bd50e5
Abrar Ahmed Shahok <abrarahmedshahok12@gmail.com>
2023-12-15 02:20:40 +0500

    Changes Profile Screen UI

--- lib/features/profile/screens/profile.dart
+++ lib/features/profile/screens/profile.dart
@@ -1,113 +1,87 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:connecthub/components/custom_app_top_bar.dart';
+import 'package:connecthub/components/custom_icon_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:iconly/iconly.dart';
 import '/features/posts/screens/saved_posts_screen.dart';
 import '../../auth/bloc/auth_bloc.dart';
-import '/components/custom_elevated_button.dart';
 import '/repos/auth_repo.dart';
 import '../../../constants/constants.dart';
 
 class Profile extends StatelessWidget {
-  Profile({super.key, required this.authBloc});
+  const Profile({super.key, required this.authBloc});
   final AuthBloc authBloc;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: MyColors.primaryColor,
-      appBar: AppBar(
-        backgroundColor: MyColors.primaryColor,
-        title: Text(
-          AuthRepo.currentUser!.username.toLowerCase().replaceAll(' ', '_'),
-          style: MyFonts.firaSans(
-            fontColor: MyColors.secondaryColor,
-          ),
-        ),
-      ),
-      body: BlocConsumer<AuthBloc, AuthState>(
-        bloc: authBloc,
-        listenWhen: (previous, current) => current is AuthActionState,
-        buildWhen: (previous, current) => current is! AuthActionState,
-        listener: (context, state) {
-          if (state is AuthNavigateToSavedPostScreenActionState) {
-            Navigator.pushNamed(context, SavedPostsScreen.routeName);
-          }
-        },
-        builder: (context, state) {
-          return Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10),
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.center,
+      body: Stack(
+        children: [
+          BlocConsumer<AuthBloc, AuthState>(
+            bloc: authBloc,
+            listenWhen: (previous, current) => current is AuthActionState,
+            buildWhen: (previous, current) => current is! AuthActionState,
+            listener: (context, state) {
+              if (state is AuthNavigateToSavedPostScreenActionState) {
+                Navigator.pushNamed(context, SavedPostsScreen.routeName);
+              }
+            },
+            builder: (context, state) {
+              return Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 10, vertical: 80),
+                child: Column(
                   children: [
-                    Column(
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
-                        CircleAvatar(
-                          radius: 40,
-                          backgroundImage: CachedNetworkImageProvider(
-                            AuthRepo.currentUser!.userImage,
-                          ),
+                        Column(
+                          children: [
+                            CircleAvatar(
+                              radius: 40,
+                              backgroundImage: CachedNetworkImageProvider(
+                                AuthRepo.currentUser!.userImage,
+                              ),
+                            ),
+                            const SizedBox(height: 5),
+                            Text(
+                              AuthRepo.currentUser!.username,
+                              style: MyFonts.firaSans(
+                                fontColor: MyColors.secondaryColor,
+                                fontSize: 18,
+                              ),
+                            ),
+                          ],
                         ),
-                        const SizedBox(height: 5),
-                        Text(
-                          AuthRepo.currentUser!.username,
-                          style: MyFonts.firaSans(
-                            fontColor: MyColors.secondaryColor,
-                            fontSize: 18,
-                          ),
-                        ),
+                        customColumn(count: 13, title: 'Posts'),
+                        customColumn(count: 262, title: 'Followers'),
+                        customColumn(count: 200, title: 'Following'),
                       ],
                     ),
-                    customColumn(count: 13, title: 'Posts'),
-                    customColumn(count: 262, title: 'Followers'),
-                    customColumn(count: 200, title: 'Following'),
                   ],
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    CustomElevatedButton(
-                      title: 'Logout',
-                      onPressed: () {
-                        authBloc.add(AuthLogoutButtonClickedEvent());
-                      },
-                    ),
-                    const SizedBox(width: 10),
-                    GestureDetector(
-                      onTap: () {
-                        authBloc.add(
-                            AuthNavigateToSavedPostScreenButtonClickedEvent());
-                      },
-                      child: Container(
-                        height: 30,
-                        width: 120,
-                        alignment: Alignment.center,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(8),
-                          border: Border.all(
-                            color: MyColors.secondaryColor,
-                          ),
-                        ),
-                        child: Text(
-                          'Saved',
-                          style: MyFonts.firaSans(
-                            fontColor: MyColors.secondaryColor,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                Divider(
-                  color: MyColors.secondaryColor,
-                  thickness: 0.1,
-                  height: 0.05,
-                ),
-              ],
+              );
+            },
+          ),
+          Positioned(
+            left: 7,
+            right: 7,
+            top: 20,
+            child: CustomAppTopBar(
+              title: 'Welcome ${AuthRepo.currentUser!.username}',
+              showActionButton: true,
+              actionButton: CustomIconButton(
+                icon: IconlyLight.logout,
+                color: Colors.white,
+                onPressed: () {
+                  authBloc.add(AuthLogoutButtonClickedEvent());
+                },
+              ),
             ),
-          );
-        },
+          ),
+        ],
       ),
     );
   }
