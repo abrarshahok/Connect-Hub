d1003864b3d651d6900656c2c33c1cf9ce2762bb
Abrar Ahmed Shahok <abrarahmedshahok12@gmail.com>
2023-12-15 19:19:59 +0500

    Added Feature to Show user Posts in Profile Screen

--- lib/features/profile/screens/profile.dart
+++ lib/features/profile/screens/profile.dart
@@ -1,9 +1,12 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:connecthub/components/custom_app_top_bar.dart';
 import 'package:connecthub/components/custom_icon_button.dart';
+import 'package:connecthub/features/posts/widgets/post_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:iconly/iconly.dart';
+import '../../../models/post_data_model.dart';
 import '/features/posts/screens/saved_posts_screen.dart';
 import '../../auth/bloc/auth_bloc.dart';
 import '/repos/auth_repo.dart';
@@ -12,8 +15,15 @@ import '../../../constants/constants.dart';
 class Profile extends StatelessWidget {
   const Profile({super.key, required this.authBloc});
   final AuthBloc authBloc;
+
   @override
   Widget build(BuildContext context) {
+    final postStream = FirebaseFirestore.instance
+        .collection('posts')
+        .where('userId', isEqualTo: AuthRepo.currentUser!.uid)
+        .orderBy('postedOn', descending: true)
+        .snapshots();
+
     return Scaffold(
       backgroundColor: MyColors.primaryColor,
       body: Stack(
@@ -28,40 +38,83 @@ class Profile extends StatelessWidget {
               }
             },
             builder: (context, state) {
-              return Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 10, vertical: 80),
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Column(
-                          children: [
-                            CircleAvatar(
-                              radius: 40,
-                              backgroundImage: CachedNetworkImageProvider(
-                                AuthRepo.currentUser!.userImage,
+              return CustomScrollView(
+                slivers: [
+                  SliverToBoxAdapter(
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                        top: 80,
+                        left: 10,
+                        right: 10,
+                        bottom: 10,
+                      ),
+                      child: Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Column(
+                                children: [
+                                  CircleAvatar(
+                                    radius: 40,
+                                    backgroundImage: CachedNetworkImageProvider(
+                                      AuthRepo.currentUser!.userImage,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 5),
+                                  Text(
+                                    AuthRepo.currentUser!.username,
+                                    style: MyFonts.bodyFont(
+                                      fontColor: MyColors.secondaryColor,
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                ],
                               ),
-                            ),
-                            const SizedBox(height: 5),
-                            Text(
-                              AuthRepo.currentUser!.username,
-                              style: MyFonts.firaSans(
-                                fontColor: MyColors.secondaryColor,
-                                fontSize: 18,
-                              ),
-                            ),
-                          ],
-                        ),
-                        customColumn(count: 13, title: 'Posts'),
-                        customColumn(count: 262, title: 'Followers'),
-                        customColumn(count: 200, title: 'Following'),
-                      ],
+                              customColumn(count: 13, title: 'Posts'),
+                              customColumn(count: 262, title: 'Followers'),
+                              customColumn(count: 200, title: 'Following'),
+                            ],
+                          ),
+                          Divider(
+                              color: MyColors.secondaryColor, thickness: 0.1),
+                        ],
+                      ),
                     ),
-                  ],
-                ),
+                  ),
+                  StreamBuilder(
+                    stream: postStream,
+                    builder: (context, snapshots) {
+                      if (snapshots.connectionState ==
+                          ConnectionState.waiting) {
+                        return SliverToBoxAdapter(
+                          child: Center(
+                            child: CircularProgressIndicator(
+                                color: MyColors.buttonColor1),
+                          ),
+                        );
+                      }
+                      final postDocuments = snapshots.data!.docs;
+                      return SliverList(
+                        delegate: SliverChildBuilderDelegate(
+                          childCount: postDocuments.length,
+                          (context, index) {
+                            final postInfo = PostDataModel.fromJson(
+                              postDocuments[index].data(),
+                              postDocuments[index].id,
+                            );
+                            return PostCard(
+                              postDataModel: postInfo,
+                              isSaved: true,
+                            );
+                          },
+                        ),
+                      );
+                    },
+                  )
+                ],
               );
             },
           ),
@@ -70,7 +123,7 @@ class Profile extends StatelessWidget {
             right: 7,
             top: 20,
             child: CustomAppTopBar(
-              title: 'Welcome ${AuthRepo.currentUser!.username}',
+              title: 'Howdy :)',
               showActionButton: true,
               actionButton: CustomIconButton(
                 icon: IconlyLight.logout,
@@ -94,7 +147,7 @@ class Profile extends StatelessWidget {
       children: [
         Text(
           count.toString(),
-          style: MyFonts.firaSans(
+          style: MyFonts.logoFont(
             fontColor: MyColors.secondaryColor,
             fontSize: 18,
             fontWeight: FontWeight.bold,
@@ -102,12 +155,46 @@ class Profile extends StatelessWidget {
         ),
         Text(
           title,
-          style: MyFonts.firaSans(
+          style: MyFonts.bodyFont(
             fontColor: MyColors.secondaryColor,
-            fontWeight: FontWeight.w400,
+            fontWeight: FontWeight.w500,
           ),
         ),
       ],
     );
   }
 }
+
+// SliverToBoxAdapter(
+                  //   child: StreamBuilder(
+                  //     stream: postStream,
+                  //     builder: (context, snapshots) {
+                  //       if (snapshots.connectionState ==
+                  //           ConnectionState.waiting) {
+                  //         return SliverToBoxAdapter(
+                  //           child: Center(
+                  //             child: CircularProgressIndicator(
+                  //               color: MyColors.buttonColor1,
+                  //             ),
+                  //           ),
+                  //         );
+                  //       }
+                        // final postDocuments = snapshots.data!.docs;
+                        // return SliverList(
+                        //   delegate: SliverChildBuilderDelegate(
+                        //     childCount: postDocuments.length,
+                        //     (context, index) {
+                        //       final postInfo = PostDataModel.fromJson(
+                        //         postDocuments[index].data(),
+                        //         postDocuments[index].id,
+                        //       );
+                        //       return PostCard(
+                        //         postDataModel: postInfo,
+                        //         isSaved: true,
+                        //       );
+                        //     },
+                        //   ),
+                        // );
+                  //     },
+                  //   ),
+                  // ),
